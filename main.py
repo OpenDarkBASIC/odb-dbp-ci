15c6cb769fdf14e69ff21e6f625ad14e67774565
TheComet <alex.murray@gmx.ch>
2021-02-17 00:59:32 +0100

    Implemented caching

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import hashlib
 import hmac
 import aiohttp
 import re
+import shutil
 
 
 if not os.path.exists("config.json"):
@@ -20,24 +21,24 @@ if not os.path.exists("config.json"):
                 "type": "dbp",
                 "platform": "windows",
                 "endpoints": {
-                    "update": "http://127.0.0.1:8014/update",
-                    "compile_multi": "http://127.0.0.1:8014/compile_multi",
-                    "commit_hash": "http://127.0.0.1:8014/commit_hash"
+                    "update": "http://127.0.0.1:8015/update",
+                    "compile": "http://127.0.0.1:8015/compile",
+                    "commit_hash": "http://127.0.0.1:8015/commit_hash"
                 },
             },
             {
                 "type": "odb",
                 "platform": "linux",
                 "endpoints": {
-                    "update": "http://127.0.0.1:8014/update",
-                    "compile_multi": "http://127.0.0.1:8014/compile_multi",
-                    "commit_hash": "http://127.0.0.1:8014/commit_hash"
+                    "update": "http://127.0.0.1:8015/update",
+                    "compile": "http://127.0.0.1:8015/compile",
+                    "commit_hash": "http://127.0.0.1:8015/commit_hash"
                 }
             }
         ],
         "odb-bot": {
             "endpoints": {
-                "status": ""
+                "status": "http://127.0.0.1:8013/odb-dbp-ci/status"
             }
         },
         "git": {
@@ -63,14 +64,14 @@ config = json.loads(open("config.json", "rb").read().decode("utf-8"))
 app = quart.Quart(__name__)
 lock = asyncio.Lock()
 
-cachedir = "cache"
+cachedir = os.path.abspath("cache")
 srcdir = os.path.join(cachedir, "sources")
-workdir = os.path.join(cachedir, "work")
+resultsdir = os.path.join(cachedir, "results")
 
 if not os.path.exists(cachedir):
     os.mkdir(cachedir)
-if not os.path.exists(workdir):
-    os.mkdir(workdir)
+if not os.path.exists(resultsdir):
+    os.mkdir(resultsdir)
 
 
 def verify_signature(payload, signature, secret):
@@ -189,36 +190,81 @@ async def do_run_all():
                     continue
             files_to_compile.append((f, basename))
 
-        # Compile the code on all available compilers
-        async def do_compile(compiler, file_list):
-            code_list = (open(filename, "rb").read().decode("utf-8") for filename in file_list)
-            code_payloads = [{"code": code} for code in code_list]
-            payload = json.dumps(code_payloads).encode("utf-8")
+        # Get compiler versions (commit hashes)
+        async def query_compiler_versions(compiler):
             try:
                 async with aiohttp.ClientSession() as session:
-                    async with session.post(url=compiler["endpoints"]["compile_multi"], data=payload) as resp:
+                    async with session.get(url=compiler["endpoints"]["commit_hash"]) as resp:
                         if resp.status != 200:
                             return False, compiler, f"Endpoint for {compiler['type']}-{compiler['platform']} returned {resp.status}"
                         resp = await resp.read()
                         resp = json.loads(resp.decode("utf-8"))
-                        return True, compiler, resp
+                        return True, compiler, resp["commit_hash"]
             except:
                 return False, compiler, "Failed to connect to endpoint"
+        version_results = await asyncio.gather(*[
+            query_compiler_versions(compiler)
+            for compiler in config["compilers"]])
+
+        # Sort into compiler type/platform
+        compiler_versions = dict()
+        for success, compiler, version_or_error in version_results:
+            if not success:
+                await post_status_to_bot(version_or_error)
+                return False
+            compiler_versions.setdefault(compiler["type"], dict())[compiler["platform"]] = version_or_error
+
+        # Make sure all ODB compilers have the same version
+        if not all_equal(version for plat, version in compiler_versions["odb"].items()):
+            versions = [f"odb/{plat}: {version}" for plat, version in compiler_versions["odb"].items()]
+            await post_status_to_bot("CI aborted because not all ODB compilers are on the same version\n" + "\n".join(versions))
+            return False
+
+        # Compile the code on all available compilers
+        async def do_compile(compiler, filename):
+            # check to see if the result was cached earlier
+            compiler_type = compiler["type"]
+            compiler_plat = compiler["platform"]
+            compiler_version = compiler_versions[compiler_type][compiler_plat]
+            basename = os.path.basename(filename)
+            cached_outdir = os.path.join(resultsdir, compiler_version, compiler_type, compiler_plat)
+            cached_outfile = os.path.join(cached_outdir, basename.replace(".dba", ".out"))
+            if os.path.exists(cached_outfile):
+                return True, compiler, filename, json.loads(open(cached_outfile, "rb").read().decode("utf-8"))
+
+            if not os.path.exists(cached_outdir):
+                os.makedirs(cached_outdir)
+
+            # Otherwise send code to endpoint for compilation
+            code_payload = json.dumps({
+                "code": open(filename, "rb").read().decode("utf-8")
+            }).encode("utf-8")
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(url=compiler["endpoints"]["compile"], data=code_payload) as resp:
+                        if resp.status != 200:
+                            return False, compiler, filename, f"Endpoint for {compiler['type']}-{compiler['platform']} returned {resp.status}"
+                        resp = await resp.read()
+                        open(cached_outfile, "wb").write(resp)
+                        resp = json.loads(resp.decode("utf-8"))
+                        return True, compiler, filename, resp
+            except:
+                return False, compiler, filename, "Failed to connect to endpoint"
         compile_results = await asyncio.gather(*[
-            do_compile(compiler, [filename for filename, basename in files_to_compile])
+            do_compile(compiler, filename)
+            for filename, basename in files_to_compile
             for compiler in config["compilers"]])
 
         # sort compiler results by filename/compiler type/platform
         d = dict()
-        for success, compiler, response in compile_results:
+        for success, compiler, filename, response in compile_results:
             if not success:
                 await post_status_to_bot(response)
                 return False
-            for filename, basename, single_response in (list(fb)+[r] for fb, r in zip(files_to_compile, response)):
-                d.setdefault(filename, dict()).setdefault(compiler["type"], dict()).setdefault(compiler["platform"], {
-                    "success": single_response["success"],
-                    "output": single_response["output"]
-                })
+            d.setdefault(filename, dict()).setdefault(compiler["type"], dict()).setdefault(compiler["platform"], {
+                "success": response["success"],
+                "output": response["output"]
+            })
         compile_results = d
 
         for filename, compiler_types in compile_results.items():
@@ -409,13 +455,13 @@ async def do_run_all():
                     "file": f,
                     "message": "BUG: Unknown/unsupported filename"
                 })
-    open(os.path.join(workdir, "report.json"), "wb").write(json.dumps(report).encode("utf-8"))
+    open(os.path.join(cachedir, "report.json"), "wb").write(json.dumps(report).encode("utf-8"))
     return True
 
 
 async def do_status():
     async with lock:
-        report = json.loads(open(os.path.join(workdir, "report.json"), "rb").read().decode("utf-8"))
+        report = json.loads(open(os.path.join(cachedir, "report.json"), "rb").read().decode("utf-8"))
 
     failed_count = len(report["failed"])
     success_count = len(report["succeeded"])
@@ -423,11 +469,11 @@ async def do_status():
 
     msgs = [f"{success_count}/{total_count} test cases succeeded"]
     for failed in report["failed"][:2]:
-        msgs.append(f"{failed['file']}\n{failed['message']}")
+        msgs.append(f"{os.path.basename(failed['file'])}\n{failed['message']}")
     await post_status_to_bot("\n\n".join(msgs))
 
 
-@app.route("/ci-sources-push", methods=["POST"])
+@app.route("/ci_sources_push", methods=["POST"])
 async def github_event_ci_sources_pushed():
     payload = await quart.request.get_data()
     if not verify_signature(payload, quart.request.headers["X-Hub-Signature-256"].replace("sha256=", ""), config["github"]["secret"]):
@@ -447,7 +493,7 @@ async def github_event_ci_sources_pushed():
     return ""
 
 
-@app.route("/odbc-push", methods=["POST"])
+@app.route("/odbc_push", methods=["POST"])
 async def github_event_odb_pushed():
     payload = await quart.request.get_data()
     if not verify_signature(payload, quart.request.headers["X-Hub-Signature-256"].replace("sha256=", ""), config["github"]["secret"]):
@@ -465,22 +511,76 @@ async def github_event_odb_pushed():
     return ""
 
 
-@app.route("/pull-sources")
+@app.route("/pull_sources")
 async def pull_sources():
     await do_pull_sources()
-    return "", 200
+    await post_status_to_bot("Sources pulled")
+    return ""
 
 
-@app.route("/run-all")
+@app.route("/update_odb")
+async def update_odb():
+    await post_status_to_bot("Updating ODB on all targets...")
+
+    async def request_update(compiler):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url=compiler["endpoints"]["update"]) as resp:
+                    if resp.status != 200:
+                        return False, compiler, f"Endpoint returned {resp.status}"
+                    resp = await resp.read()
+                    return True, compiler, json.loads(resp.decode("utf-8"))
+        except:
+            return False, compiler, "Failed to connect to endpoint"
+    results = await asyncio.gather(*[request_update(compiler) for compiler in config["compilers"]])
+
+    if not all(success for success, compiler, result in results):
+        await post_status_to_bot(f"Failed to update ODB on some targets: {results[0][2]}")
+        return ""
+
+    if not all(result["success"] for success, compiler, result in results):
+        failed_msgs = [f"{compiler['type']}/{compiler['platform']}: {result['message']}"
+                       for success, compiler, result in results]
+        await post_status_to_bot("Failed to update ODB on some targets:\n" + "\n".join(failed_msgs))
+        return ""
+
+    try:
+        first_odb_compiler = [c for c in config["compilers"] if c["type"] == "odb"][0]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=first_odb_compiler["endpoints"]["commit_hash"]) as resp:
+                if resp.status != 200:
+                    await post_status_to_bot(f"Endpoint returned {resp.status}")
+                    return ""
+                resp = await resp.read()
+                version = json.loads(resp.decode("utf-8"))["commit_hash"]
+                await post_status_to_bot(f"ODB updated to {version}")
+                return ""
+    except:
+        await post_status_to_bot("Failed to connect to endpoint")
+        return ""
+
+
+@app.route("/clear_cache")
+async def clear_cache():
+    async with lock:
+        shutil.rmtree(resultsdir)
+        os.mkdir(resultsdir)
+    await post_status_to_bot("Cache cleared")
+    return ""
+
+
+@app.route("/run_all")
 async def run_all():
+    await post_status_to_bot("CI run started")
     await do_run_all()
-    return "", 200
+    await do_status()
+    return ""
 
 
 @app.route("/status")
 async def status():
     await do_status()
-    return "", 200
+    return ""
 
 
 loop = asyncio.get_event_loop()
